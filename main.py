f14bb35f43c9d4d7e0dc53efbb06f3c92eaf6052
artlink52 <ironosov.ironosov@mail.ru>
2024-06-23 01:08:01 +0300

    third initial

--- main.py
+++ main.py
@@ -1,56 +1,41 @@
-# from fastapi import FastAPI, Request
-# from fastapi.responses import HTMLResponse
-# from fastapi.staticfiles import StaticFiles
-# from fastapi.templating import Jinja2Templates
-# from pydantic import BaseModel
-# from typing import List
-# import uvicorn
-
-# app = FastAPI()
-
-# templates = Jinja2Templates(directory="templates")
-
-# class DataModel(BaseModel):
-#     keywords: List[str]
-
-# # Тестовые данные
-# test_data = [
-#     {"keywords": ["ааа"], "option": "Все слова", "places": ["resume_title"]},
-#     {"keywords": ["ббб"], "option": "Любое из слов", "places": ["education", "skills"]}
-# ]
-
-# @app.get("/", response_class=HTMLResponse)
-# async def read_root(request: Request):
-#     return templates.TemplateResponse("1str.html", {"request": request})
-
-# @app.post("/submit", response_class=HTMLResponse)
-# async def submit_data(data: DataModel, request: Request):
-#     test_data.append(data.model_dump())
-#     return {"message": "Data received successfully"}
-
-# if __name__ == "__main__":
-#     uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
-
 from fastapi import FastAPI, Request, Form, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from typing import Optional
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse, PlainTextResponse
+import json
+from pydantic import BaseModel
+from typing import Dict, List, Optional
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
 
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-vacancies = {
-    "19d9a2d3-89ba-b21c-a4eb-65f99d5dec19_Фрезеровщик": {
-        "value": "Изготовление сложно профильных деталей с точностью размеров по 4 - 6 квалификациям, сложных деталей с точностью размеров по 6, 7 квалификациям на фрезерных станках.\nОпыт работы в изготовлении режущего инструмента",
-        "number": "talentforce"
-    }
-}
+with open('vacancies.json', 'r', encoding='utf-8') as f:
+    vacancies = json.load(f)
+
+with open('resumes.json', 'r', encoding='utf-8') as f:
+    resumes = json.load(f)
+
+class Job(BaseModel):
+    company_name: str
+    post: str
+    post_desc: str
+    period_begin: str
+    period_end: Optional[str]
+
+class Education(BaseModel):
+    university: str
+    specialization: str
+    year_of_graduation: str
+
+class CandidateInfo(BaseModel):
+    education: List[Education]
+    job: List[Job]
+
 
 @app.get("/1str", response_class=HTMLResponse)
 async def get_first_page(request: Request):
@@ -68,7 +53,15 @@ async def get_vacancy(request: Request, vacancy_key: str):
     if vacancy_key not in vacancies:
         raise HTTPException(status_code=404, detail="Вакансия не найдена")
     vacancy = vacancies[vacancy_key]
-    return templates.TemplateResponse("2str.html", {"request": request, "vacancy_number": vacancy["number"], "vacancy_value": vacancy["value"]})
+    return templates.TemplateResponse("2str.html", {"request": request, "vacancy_number": vacancy_key, "vacancy_value": vacancy["value"], "data": resumes})
+
+
+
+
+
+# @app.get("/", response_class=HTMLResponse)
+# async def index(request: Request):
+#     return templates.TemplateResponse("2str.html", {"request": request, "data": resumes})
 
 
 
--- static/js/2str.js
+++ static/js/2str.js
@@ -1,51 +1,50 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleButtons = document.querySelectorAll('.toggle-button');
+    const countSelect = document.getElementById('count-talent');
+    const resumes = document.querySelectorAll('.resume_and_func');
 
-// отображение количества резюме талентфорсе
-const toggleButtons = document.querySelectorAll('.toggle-button');
-        const countSelect = document.getElementById('count-talent');
-        const resumes = document.querySelectorAll('.resume_and_func');
+    toggleButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            const resumeContainer = button.previousElementSibling;
+            resumeContainer.classList.toggle('expanded');
+            if (resumeContainer.classList.contains('expanded')) {
+                button.textContent = 'Скрыть';
+            } else {
+                button.textContent = 'Показать больше';
+            }
 
-        toggleButtons.forEach(button => {
-            button.addEventListener('click', () => {
-                const resumeContainer = button.previousElementSibling;
-                resumeContainer.classList.toggle('expanded');
-                if (resumeContainer.classList.contains('expanded')) {
-                    button.textContent = 'Скрыть';
-                } else {
-                    button.textContent = 'Показать больше';
-                }
-            });
+            // Проверяем высоту .resume
+            if (resumeContainer.clientHeight < 200) {
+                button.style.display = 'none'; 
+            } else {
+                button.style.display = 'block'; 
+            }
         });
 
-        function updateResumeDisplay() {
-            const selectedValue = countSelect.value;
-            let resumeCount = selectedValue === 'all' ? resumes.length : parseInt(selectedValue);
+        // Проверяем высоту .resume при инициализации
+        const resumeContainer = button.previousElementSibling;
+        if (resumeContainer.clientHeight < 200) {
+            button.style.display = 'none'; 
+        }
+    });
 
-            for (let i = 0; i < resumes.length; i++) {
-                if (i < resumeCount) {
-                    resumes[i].style.display = 'block';
-                } else {
-                    resumes[i].style.display = 'none';
-                }
+    function updateResumeDisplay() {
+        const selectedValue = countSelect.value;
+        let resumeCount = selectedValue === 'all' ? resumes.length : parseInt(selectedValue);
+
+        for (let i = 0; i < resumes.length; i++) {
+            if (i < resumeCount) {
+                resumes[i].style.display = 'block';
+            } else {
+                resumes[i].style.display = 'none';
             }
         }
+    }
 
-        countSelect.addEventListener('change', () => {
-            updateResumeDisplay();
-        });
-
-        // Инициализация
+    countSelect.addEventListener('change', () => {
         updateResumeDisplay();
+    });
 
-// отображение количества резюме в бд
-
-//
-// const vacancyNumber = sessionStorage.getItem('vacancyNumber');
-//         const vacancyValue = sessionStorage.getItem('vacancyValue');
-
-//         // Отобразить данные на странице
-//         if (vacancyNumber && vacancyValue) {
-//             document.querySelector('.num-vac').textContent = `Номер вакансии: ${vacancyNumber}`;
-//             document.querySelector('.desc-vac').textContent = `Описание вакансии: ${vacancyValue}`;
-//         } else {
-//             document.querySelector('.vacs').innerHTML = '<p>Вакансия не найдена</p>';
-//         }
+    // Инициализация
+    updateResumeDisplay();
+});
